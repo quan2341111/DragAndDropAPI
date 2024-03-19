5be1486f28dc663badfcf8aa67225b7129f5b731
anhquandev <huynh.anh.quan.vn.com@gmail.com>
2024-03-19 15:03:56 +0700

    update_deploy

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,8 +1,6 @@
 //Những domain được truy cập tới server
-export const WHITELIST_DOMAINS = [
-  // 'http://localhost:5173' (không cần locahost nữa vì ở file config/cors đã luôn luôn cho phép môi trường dev env.BUILD_MODE === 'dev')
-]
-
+export const WHITELIST_DOMAINS = []
+// 'http://localhost:5173' (không cần locahost nữa vì ở file config/cors đã luôn luôn cho phép môi trường dev env.BUILD_MODE === 'dev')
 
 export const BOARD_TYPES = {
   PUBLIC: 'public',
