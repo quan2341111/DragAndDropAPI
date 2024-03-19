5057e00ec4741678993618f12fcda115e778a985
anhquandev <huynh.anh.quan.vn.com@gmail.com>
2024-03-19 21:03:13 +0700

    charge_WHITELIST_DOMAIN

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,5 +1,7 @@
 //Những domain được truy cập tới server
-export const WHITELIST_DOMAINS = []
+export const WHITELIST_DOMAINS = [
+  'https://website-one-mocha-38.vercel.app/'
+]
 // 'http://localhost:5173' (không cần locahost nữa vì ở file config/cors đã luôn luôn cho phép môi trường dev env.BUILD_MODE === 'dev')
 
 export const BOARD_TYPES = {
