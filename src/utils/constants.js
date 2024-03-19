ac1e2fddf6b456485aa0a0eaf97ea7b783c0ecce
anhquandev <huynh.anh.quan.vn.com@gmail.com>
2024-03-19 14:34:34 +0700

    Update_CORD_DEPLOY

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,6 +1,6 @@
 //Những domain được truy cập tới server
 export const WHITELIST_DOMAINS = [
-  'http://localhost:5173'
+  // 'http://localhost:5173' (không cần locahost nữa vì ở file config/cors đã luôn luôn cho phép môi trường dev env.BUILD_MODE === 'dev')
 ]
 
 
